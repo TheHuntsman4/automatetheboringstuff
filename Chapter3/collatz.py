38780a8b83842a9adf9bd136a8c9122d2e6dc247
TheHuntsman4 <anikethvij464@gmail.com>
2023-01-08 16:29:32 +0530

    chapter3

---
+++ Chapter3/collatz.py
@@ -0,0 +1,13 @@
+import sys
+def collatz(number):
+        if(number%2==0):
+            number=number//2
+        else:
+            number=3*number+1
+        print(number)
+        while(number!=1):
+            collatz(number)
+        sys.exit()
+
+n=int(input())
+collatz(n)
